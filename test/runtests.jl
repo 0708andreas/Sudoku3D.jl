6562aa7c842879ea44daf863aae0d366515dbad1
Thomas Poulsen <thomas@lha66.dk>
2023-07-05 16:49:36 +0200

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using Sudoku3D
 using Test
 
+g= "..4..1.............................4.............1.............."
+s = sort(search(init_multi(g)))
+
 @testset "Sudoku3D.jl" begin
-    # Write your tests here.
+    @test s[1] == "2341412312343412143232144321214332141432214343214123234134121234"
 end
