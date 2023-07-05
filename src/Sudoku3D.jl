7f6649bd354728fc5c4a75b5d4ca9847b5c260f3
Thomas Poulsen <thomas@lha66.dk>
2023-07-05 16:44:39 +0200

    now with code

--- src/Sudoku3D.jl
+++ src/Sudoku3D.jl
@@ -1,5 +1,7 @@
 module Sudoku3D
 
-# Write your package code here.
+include("solver.jl")
+
+export vals, entries, dims, SudokuPuzzle, SudokuPartial, squares, rows, cols, fils, subs, unitslist, units, peers, assign!, eliminate!, propagate!, search, init_multi, rand_puzzle
 
 end
