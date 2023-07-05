48d1de568668340d6a7eafc19a9cc5131a39bfa1
Documenter.jl <documenter@juliadocs.github.io>
2023-07-05 14:18:51 +0000

    build based on 5eff025

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Sudoku3D","category":"page"},{"location":"#Sudoku3D","page":"Home","title":"Sudoku3D","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Sudoku3D.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Sudoku3D]","category":"page"}]
+}
